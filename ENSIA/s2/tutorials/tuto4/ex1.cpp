ecb3326fce6dfaa4bf7e7d7943b1316020f13e2f
zdx <typozaki12@gmail.com>
2024-02-28 18:26:22 +0100

    Create ex1.cpp

---
+++ ENSIA/s2/tutorials/tuto4/ex1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class country{
+    private:
+        string name;
+        double population, area;
+        capital capitalCity;
+        double totalPopulation;
+    
+    public: 
+        country(string nm, double pop, double ar,  double totalPop, capital capCt){
+            name = nm;
+            population = pop;
+            area = ar;
+            totalPop = totalPopulation
+            capitalCity = capCt;
+        }
+        
+
+};
+class capital{
+
+};
+
+int main(){
+
+    return 0;
+}
