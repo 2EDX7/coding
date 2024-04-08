880e22fd334c783f61e8ad02fc23cc5839e0e669
zdx <typozaki12@gmail.com>
2024-04-08 15:39:24 +0100

    ignoring python learning files

--- test.cpp
+++ test.cpp
@@ -1,15 +1,7 @@
 #include "cp.h"
 using namespace std;
-void post_increment(int a){
-    cout<< ++a <<endl;
-}
-void pre_increment(int a){
-    cout<< a++ <<endl;
-}
 
-
-int main (){
-    post_increment(5);
-    pre_increment(5);
-    return 0;
+int main() {
+    string password;
+    cin >> password;
 }
